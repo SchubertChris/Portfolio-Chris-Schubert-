47769a83fb6517bec371fd3842b9e584cf6854e2
SchubertChris <schubert_chris@rocketmail.com>
2025-04-02 18:01:48 +0200

    Vite config edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
   
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     
-    // Dynamische Base-Konfiguration
     base: isProduction 
       ? '/Portfolio-Chris-Schubert-/' 
       : '/',
     
+    resolve: {
+      alias: {
+      }
+    },
+    
     build: {
       outDir: 'dist',
-      // Zusätzliche Build-Optimierungen
       minify: isProduction,
       sourcemap: !isProduction
     },
     
-    // Entwicklungsserver-Konfiguration
     server: {
       port: 3000,
       open: true
-    },
-    
-    // Anpassungen für verschiedene Umgebungen
-    define: {
-      'process.env.NODE_ENV': JSON.stringify(mode)
     }
   }
 })
