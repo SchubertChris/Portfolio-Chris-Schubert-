ca2105b3c2957cf8de132d35d72d3217745ec0c4
SchubertChris <schubert_chris@rocketmail.com>
2025-04-04 01:20:20 +0200

    Vite Build konfiguriert für Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/Portfolio-Chris-Schubert-',
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
-  // Fügen Sie MIME-Typ-Konfiguration hinzu
+  base: '/',
   resolve: {
     alias: {
       '@': '/src',
