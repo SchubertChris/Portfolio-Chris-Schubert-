89ed565d985b88b499fbff4b9e007a7b647ee537
SchubertChris <schubert_chris@rocketmail.com>
2025-04-06 22:17:53 +0200

    Menu und Hero Sec bearbeitet Scrollverhalten verbessert und nav höher gesetzt

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from './contexts/ThemeContext' // Neuer Import
 import './index.scss'
+import './styles/abstracts/_theme.scss' // Neuer Import
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </HelmetProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
